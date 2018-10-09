76df964f35c3b40f767f288e7d093f82183362bb
Kshitij Yadav <kshitij.yadav@threadsol.com>
2018-10-09 12:11:15 +0530

    add change remote method

--- lib/generators/git_scratch/common/my_methods.rb
+++ lib/generators/git_scratch/common/my_methods.rb
@@ -6,13 +6,49 @@ module CommonMethods
   included do
     desc 'GitScratch rails app'
 
-    argument :new_name, :type => :string, :default => ''
+    argument :new_remote_path, :type => :string, :default => ''
   end
 
   protected
 
   def perform
     puts "perform in commons....GitScratch"
+    @remote_path = new_name.to_s.strip
+    change_remote_path
+  end
+
+  def get_choice
+    flag = STDIN.gets.strip.downcase
+    if flag == 'n'
+      return false
+    elsif flag == 'y'
+      return true
+    else
+      puts "Please enter 'y' or 'n' :"
+      get_choice
+    end
+  end
+
+  def change_remote_path
+    begin
+      `git remote remove origin`
+      if @remote_path.blank?
+        puts "Want to add new remote path(y/n):"
+        if get_choice
+          @remote_path = STDIN.gets.strip.downcase  
+          puts "Invalid remote path." if @remote_path.blank?
+        end
+      end  
+      
+      puts "Changing remote path"
+      if @remote_path.present?
+        `git remote add origin #{@remote_path}`
+      end
+      puts "Current remote paths ->"
+      `git remote -v`
+    rescue => e
+      puts "error => #{e}"
+    end
   end
 
 end
