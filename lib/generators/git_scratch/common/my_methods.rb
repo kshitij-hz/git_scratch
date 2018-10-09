d499c0601e4248b2d851be68cdca47559e5a8e37
Kshitij Yadav <kshitij.yadav@threadsol.com>
2018-10-09 12:38:44 +0530

    reset  commits

--- lib/generators/git_scratch/common/my_methods.rb
+++ lib/generators/git_scratch/common/my_methods.rb
@@ -15,6 +15,27 @@ module CommonMethods
     puts "perform in commons....GitScratch"
     @remote_path = new_remote_path.to_s.strip
     change_remote_path
+    reset_all_commits
+  end
+
+  def reset_all_commits
+    begin
+       if get_choice("Want to reset all commits (y/n) : ")
+        puts "Resetting all commit messages"
+        current_branch = `git rev-parse --abbrev-ref HEAD`
+        puts "Current Branch => #{current_branch.to_s}"
+        `git checkout --orphan latest_branch`
+        `git add -A`
+        `git commit -am "git_scratch initial commit"`
+        `git branch -D master`
+        `git branch -m master`
+        `git push -f origin master` if @remote_path.present?
+
+        puts "\t Done..."
+      end
+    rescue => e
+      puts "error => #{e}"
+    end
   end
 
   def get_choice(msg = '')
@@ -35,7 +56,8 @@ module CommonMethods
       `git remote remove origin`
       if @remote_path.blank?
         puts "Want to add new remote path(y/n):"
-        if get_choice('Enter remote path :')
+        if get_choice
+          puts "Enter remote path :"
           @remote_path = STDIN.gets.strip.downcase  
           puts "Invalid remote path." if @remote_path.blank?
         end
@@ -45,8 +67,11 @@ module CommonMethods
       if @remote_path.present?
         `git remote add origin #{@remote_path}`
       end
-      puts "Current remote paths ->"
-      puts `git remote -v`
+      current_remotes = `git remote -v`
+      if current_remotes.to_s.strip.present?
+        puts "current remotes => \n #{current_remotes.to_s}"
+      end
+      puts "\t Done..."
     rescue => e
       puts "error => #{e}"
     end
